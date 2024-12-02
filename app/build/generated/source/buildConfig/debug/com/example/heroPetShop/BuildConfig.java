87e3eface1b0be021fe5ff2bca67cca64651ddfb
nhongg <anhntkph42900@fpt.edu.vn>
2024-12-02 22:21:21 +0700

    Update firebase all

--- app/build/generated/source/buildConfig/debug/com/example/heroPetShop/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/heroPetShop/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.heroPetShop;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.doanfood";
+  public static final String APPLICATION_ID = "com.example.heroPetShop";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";

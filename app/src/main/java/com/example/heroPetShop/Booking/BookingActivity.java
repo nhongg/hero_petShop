07cd7249312f0b29d42b5714f3a32d96e743f549
hiuhiu2302 <whysoeazy9797@gmail.com>
2024-12-22 06:36:37 +0700

    update

--- app/src/main/java/com/example/heroPetShop/Booking/BookingActivity.java
+++ app/src/main/java/com/example/heroPetShop/Booking/BookingActivity.java
@@ -39,7 +39,7 @@ public class BookingActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
     private int selectedYear, selectedMonth, selectedDay, selectedHour, selectedMinute;
-    private String selectedDate, selectedTime;
+    private String selectedDate, selectedTime,selectedTime1;
     private String tenDichVu; // Dịch vụ từ DetailServiceActivity
     private String userId; // ID người dùng
     Date thoiGianDatLichCt;
@@ -65,11 +65,16 @@ public class BookingActivity extends AppCompatActivity {
 
         TextView weightWarningText = findViewById(R.id.weightWarningText);
 
+
+
         btnca1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selectedTime = "08:00";
-                Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
+                selectedTime1 = "8:00:00 - 10:00:00";
+
+                txtChonGio.setText("Giờ đặt: " + selectedTime1);
+             //   Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -77,14 +82,20 @@ public class BookingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 selectedTime = "10:00";
-                Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
+                selectedTime1 = "10:00:00 - 12:00:00";
+
+                txtChonGio.setText("Giờ đặt: " + selectedTime1);
+               // Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
 
             }
         }); btnca3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selectedTime = "12:00";
-                Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
+                selectedTime1 = "12:00:00 - 14:00:00";
+
+                txtChonGio.setText("Giờ đặt: " + selectedTime1);
+               // Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -92,7 +103,10 @@ public class BookingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 selectedTime = "14:00";
-                Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
+                selectedTime1 = "14:00:00 - 16:00:00";
+
+                txtChonGio.setText("Giờ đặt: " + selectedTime1);
+             //   Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -100,7 +114,11 @@ public class BookingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 selectedTime = "16:00";
-                Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
+
+                selectedTime1 = "16:00:00 - 18:00:00";
+
+                txtChonGio.setText("Giờ đặt: " + selectedTime1);
+                //Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -108,7 +126,10 @@ public class BookingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 selectedTime = "18:00";
-                Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
+                selectedTime1 = "18:00:00 - 20:00:00";
+
+                txtChonGio.setText("Giờ đặt: " + selectedTime1);
+                //Toast.makeText(BookingActivity.this, ""+selectedTime, Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -207,7 +228,6 @@ public class BookingActivity extends AppCompatActivity {
 //
 //            datePickerDialog.show();
 //        });
-
         txtChonNgay.setOnClickListener(v -> {
             Calendar calendar = Calendar.getInstance();
             selectedYear = calendar.get(Calendar.YEAR);
@@ -225,7 +245,7 @@ public class BookingActivity extends AppCompatActivity {
                 Calendar selectedCalendar = Calendar.getInstance();
                 selectedCalendar.set(selectedYear, selectedMonth, selectedDay, 0, 0, 0); // Chỉ lấy ngày, không có giờ phút
                 long selectedTimeMillis = selectedCalendar.getTimeInMillis();
-                //long currentTimeMillis = System.currentTimeMillis();
+                //  long currentTimeMillis = System.currentTimeMillis();
 
                 if (selectedTimeMillis < currentTimeMillis) {
                     // Nếu thời gian chọn là quá khứ, yêu cầu chọn lại
@@ -234,35 +254,205 @@ public class BookingActivity extends AppCompatActivity {
                 } else {
                     txtChonNgay.setText("Ngày đặt: " + selectedDate);
 
-                    // Chuyển ngày chọn thành Timestamp
-                    Calendar startCalendar = Calendar.getInstance();
-                    startCalendar.set(selectedYear, selectedMonth, selectedDay, 8, 0, 0); // 8h sáng
-                    Timestamp startTimestamp = new Timestamp(startCalendar.getTime());
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 8h sáng
+                    Calendar startCalendar1 = Calendar.getInstance();
+                    startCalendar1.set(selectedYear, selectedMonth, selectedDay, 7, 59, 59); // 8h sáng
+                    Timestamp startTimestamp1 = new Timestamp(startCalendar1.getTime());
 
-                    Calendar endCalendar = Calendar.getInstance();
-                    endCalendar.set(selectedYear, selectedMonth, selectedDay, 10, 0, 0); // 10h sáng
-                    Timestamp endTimestamp = new Timestamp(endCalendar.getTime());
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 10h sáng
+                    Calendar endCalendar1 = Calendar.getInstance();
+                    endCalendar1.set(selectedYear, selectedMonth, selectedDay, 10, 0, 0); // 10h sáng
+                    Timestamp endTimestamp1 = new Timestamp(endCalendar1.getTime());
 
-                    // Thực hiện truy vấn Firestore
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 10h sáng
+                    Calendar startCalendar2 = Calendar.getInstance();
+                    startCalendar2.set(selectedYear, selectedMonth, selectedDay, 9, 59, 59); // 10h sáng
+                    Timestamp startTimestamp2 = new Timestamp(startCalendar2.getTime());
+
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 12h trưa
+                    Calendar endCalendar2 = Calendar.getInstance();
+                    endCalendar2.set(selectedYear, selectedMonth, selectedDay, 12, 0, 0); // 12h trưa
+                    Timestamp endTimestamp2 = new Timestamp(endCalendar2.getTime());
+
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 12h sáng
+                    Calendar startCalendar3 = Calendar.getInstance();
+                    startCalendar2.set(selectedYear, selectedMonth, selectedDay, 11, 59, 59); // 10h sáng
+                    Timestamp startTimestamp3 = new Timestamp(startCalendar2.getTime());
+
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 14h
+                    Calendar endCalendar3 = Calendar.getInstance();
+                    endCalendar2.set(selectedYear, selectedMonth, selectedDay, 14, 0, 0); // 12h trưa
+                    Timestamp endTimestamp3 = new Timestamp(endCalendar2.getTime());
+
+
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 14h
+                    Calendar startCalendar4 = Calendar.getInstance();
+                    startCalendar2.set(selectedYear, selectedMonth, selectedDay, 13, 59, 59); // 10h sáng
+                    Timestamp startTimestamp4 = new Timestamp(startCalendar2.getTime());
+
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 16h
+                    Calendar endCalendar4 = Calendar.getInstance();
+                    endCalendar2.set(selectedYear, selectedMonth, selectedDay, 16, 0, 0); // 12h trưa
+                    Timestamp endTimestamp4 = new Timestamp(endCalendar2.getTime());
+
+
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 16h
+                    Calendar startCalendar5 = Calendar.getInstance();
+                    startCalendar2.set(selectedYear, selectedMonth, selectedDay, 15, 59, 59); // 10h sáng
+                    Timestamp startTimestamp5 = new Timestamp(startCalendar2.getTime());
+
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 18h
+                    Calendar endCalendar5 = Calendar.getInstance();
+                    endCalendar2.set(selectedYear, selectedMonth, selectedDay, 18, 0, 0); // 12h trưa
+                    Timestamp endTimestamp5 = new Timestamp(endCalendar2.getTime());
+
+
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 18h sáng
+                    Calendar startCalendar6 = Calendar.getInstance();
+                    startCalendar2.set(selectedYear, selectedMonth, selectedDay, 17, 59, 59); // 10h sáng
+                    Timestamp startTimestamp6 = new Timestamp(startCalendar2.getTime());
+
+                    // Chuyển ngày chọn thành Timestamp cho thời gian 12h trưa
+                    Calendar endCalendar6 = Calendar.getInstance();
+                    endCalendar2.set(selectedYear, selectedMonth, selectedDay, 20, 0, 0); // 12h trưa
+                    Timestamp endTimestamp6 = new Timestamp(endCalendar2.getTime());
+
+                    // Thực hiện truy vấn Firestore cho khoảng thời gian từ 8h-10h
                     FirebaseFirestore db = FirebaseFirestore.getInstance();
                     db.collection("CTHDBooking")
-                            .whereGreaterThanOrEqualTo("thoiGianDatLich", startTimestamp)
-                            .whereLessThanOrEqualTo("thoiGianDatLich", endTimestamp)
+                            .whereGreaterThanOrEqualTo("thoiGianDatLich", startTimestamp1)
+                            .whereLessThanOrEqualTo("thoiGianDatLich", endTimestamp1)
                             .get()
                             .addOnCompleteListener(task -> {
                                 if (task.isSuccessful()) {
-                                    int numberOfInvoices = task.getResult().size();
-                                    Toast.makeText(BookingActivity.this, "Số hóa đơn trong khoảng thời gian từ 8-10h: " + numberOfInvoices, Toast.LENGTH_SHORT).show();
-                                    if (numberOfInvoices > 3) {
+                                    int numberOfInvoices1 = task.getResult().size();
+                                   // Toast.makeText(BookingActivity.this, "Số hóa đơn trong khoảng thời gian từ 8-10h: " + numberOfInvoices1, Toast.LENGTH_SHORT).show();
+                                    if (numberOfInvoices1 > 3) {
                                         btnca1.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.Red)); // Thay đổi màu sang đỏ
+                                        btnca1.setEnabled(false);
                                     } else {
-                                        btnca1.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.colorAccent)); // Màu mặc định
+                                        btnca1.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.bg_color_url)); // Màu mặc định
+                                        btnca1.setEnabled(true);
                                     }
 
 
 
                                 } else {
-                                    Toast.makeText(BookingActivity.this, "Lỗi khi truy vấn: " + task.getException(), Toast.LENGTH_SHORT).show();
+                                   // Toast.makeText(BookingActivity.this, "Lỗi khi truy vấn (8-10h): " + task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                    // Thực hiện truy vấn Firestore cho khoảng thời gian từ 10h-12h
+                    db.collection("CTHDBooking")
+                            .whereGreaterThanOrEqualTo("thoiGianDatLich", startTimestamp2)
+                            .whereLessThanOrEqualTo("thoiGianDatLich", endTimestamp2)
+                            .get()
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    int numberOfInvoices2 = task.getResult().size();
+                                   // Toast.makeText(BookingActivity.this, "Số hóa đơn trong khoảng thời gian từ 10-12h: " + numberOfInvoices2, Toast.LENGTH_SHORT).show();
+
+                                    if (numberOfInvoices2 > 3) {
+                                        btnca2.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.Red)); // Thay đổi màu sang đỏ
+                                        btnca2.setEnabled(false);
+                                    } else {
+                                        btnca2.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.bg_color_url)); // Màu mặc định
+                                        btnca2.setEnabled(true);
+                                    }
+
+                                } else {
+                                  //  Toast.makeText(BookingActivity.this, "Lỗi khi truy vấn (10-12h): " + task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                    //ca3
+                    db.collection("CTHDBooking")
+                            .whereGreaterThanOrEqualTo("thoiGianDatLich", startTimestamp3)
+                            .whereLessThanOrEqualTo("thoiGianDatLich", endTimestamp3)
+                            .get()
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    int numberOfInvoices3 = task.getResult().size();
+                                   // Toast.makeText(BookingActivity.this, "Số hóa đơn trong khoảng thời gian từ 10-12h: " + numberOfInvoices3, Toast.LENGTH_SHORT).show();
+
+                                    if (numberOfInvoices3 > 3) {
+                                        btnca3.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.Red)); // Thay đổi màu sang đỏ
+                                        btnca3.setEnabled(false);
+                                    } else {
+                                        btnca3.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.bg_color_url)); // Màu mặc định
+                                        btnca3.setEnabled(true);
+                                    }
+
+                                } else {
+                                   // Toast.makeText(BookingActivity.this, "Lỗi khi truy vấn (10-12h): " + task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                    //ca 4
+                    db.collection("CTHDBooking")
+                            .whereGreaterThanOrEqualTo("thoiGianDatLich", startTimestamp4)
+                            .whereLessThanOrEqualTo("thoiGianDatLich", endTimestamp4)
+                            .get()
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    int numberOfInvoices2 = task.getResult().size();
+                                   // Toast.makeText(BookingActivity.this, "Số hóa đơn trong khoảng thời gian từ 10-12h: " + numberOfInvoices2, Toast.LENGTH_SHORT).show();
+
+                                    if (numberOfInvoices2 > 3) {
+                                        btnca4.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.Red)); // Thay đổi màu sang đỏ
+                                        btnca4.setEnabled(false);
+                                    } else {
+                                        btnca4.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.bg_color_url)); // Màu mặc định
+                                        btnca4.setEnabled(true);
+                                    }
+
+                                } else {
+                                   // Toast.makeText(BookingActivity.this, "Lỗi khi truy vấn (10-12h): " + task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                    //ca 5
+                    db.collection("CTHDBooking")
+                            .whereGreaterThanOrEqualTo("thoiGianDatLich", startTimestamp5)
+                            .whereLessThanOrEqualTo("thoiGianDatLich", endTimestamp5)
+                            .get()
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    int numberOfInvoices2 = task.getResult().size();
+                                    //Toast.makeText(BookingActivity.this, "Số hóa đơn trong khoảng thời gian từ 10-12h: " + numberOfInvoices2, Toast.LENGTH_SHORT).show();
+
+                                    if (numberOfInvoices2 > 3) {
+                                        btnca5.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.Red)); // Thay đổi màu sang đỏ
+                                        btnca5.setEnabled(false);
+                                    } else {
+                                        btnca5.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.bg_color_url)); // Màu mặc định
+                                        btnca5.setEnabled(true);
+                                    }
+
+                                } else {
+                                  //  Toast.makeText(BookingActivity.this, "Lỗi khi truy vấn (10-12h): " + task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                    //ca 6
+                    db.collection("CTHDBooking")
+                            .whereGreaterThanOrEqualTo("thoiGianDatLich", startTimestamp6)
+                            .whereLessThanOrEqualTo("thoiGianDatLich", endTimestamp6)
+                            .get()
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    int numberOfInvoices2 = task.getResult().size();
+                                    //Toast.makeText(BookingActivity.this, "Số hóa đơn trong khoảng thời gian từ 10-12h: " + numberOfInvoices2, Toast.LENGTH_SHORT).show();
+
+                                    if (numberOfInvoices2 > 3) {
+                                        btnca6.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.Red)); // Thay đổi màu sang đỏ
+                                        btnca6.setEnabled(false);
+                                    } else {
+                                        btnca6.setBackgroundColor(ContextCompat.getColor(BookingActivity.this, R.color.bg_color_url)); // Màu mặc định
+                                        btnca6.setEnabled(true);
+                                    }
+
+                                } else {
+                                  //  Toast.makeText(BookingActivity.this, "Lỗi khi truy vấn (10-12h): " + task.getException(), Toast.LENGTH_SHORT).show();
                                 }
                             });
                 }
